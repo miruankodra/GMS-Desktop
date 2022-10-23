52a7c9411757a53c8cf6085b9116dfcf98c1163c
reizeqai <86569785+reizeqai@users.noreply.github.com>
2022-10-23 15:18:05 +0200

    Dashboard

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GMS
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopPanelDash = new System.Windows.Forms.Panel();
+            this.LeftPanelDash = new System.Windows.Forms.Panel();
+            this.MiddelPanelDash = new System.Windows.Forms.Panel();
+            this.LogoGms = new System.Windows.Forms.Label();
+            this.DashBoardBtn = new System.Windows.Forms.Button();
+            this.ClimateBtn = new System.Windows.Forms.Button();
+            this.ControlPanelBtn = new System.Windows.Forms.Button();
+            this.ModalitetBtn = new System.Windows.Forms.Button();
+            this.TopPanelDash.SuspendLayout();
+            this.LeftPanelDash.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopPanelDash
+            // 
+            this.TopPanelDash.BackColor = System.Drawing.Color.White;
+            this.TopPanelDash.Controls.Add(this.LogoGms);
+            this.TopPanelDash.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanelDash.Location = new System.Drawing.Point(0, 0);
+            this.TopPanelDash.Name = "TopPanelDash";
+            this.TopPanelDash.Size = new System.Drawing.Size(982, 100);
+            this.TopPanelDash.TabIndex = 0;
+            // 
+            // LeftPanelDash
+            // 
+            this.LeftPanelDash.BackColor = System.Drawing.Color.White;
+            this.LeftPanelDash.Controls.Add(this.ModalitetBtn);
+            this.LeftPanelDash.Controls.Add(this.ControlPanelBtn);
+            this.LeftPanelDash.Controls.Add(this.ClimateBtn);
+            this.LeftPanelDash.Controls.Add(this.DashBoardBtn);
+            this.LeftPanelDash.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanelDash.Location = new System.Drawing.Point(0, 100);
+            this.LeftPanelDash.Name = "LeftPanelDash";
+            this.LeftPanelDash.Size = new System.Drawing.Size(263, 553);
+            this.LeftPanelDash.TabIndex = 1;
+            // 
+            // MiddelPanelDash
+            // 
+            this.MiddelPanelDash.BackColor = System.Drawing.Color.White;
+            this.MiddelPanelDash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MiddelPanelDash.Location = new System.Drawing.Point(263, 100);
+            this.MiddelPanelDash.Name = "MiddelPanelDash";
+            this.MiddelPanelDash.Size = new System.Drawing.Size(719, 553);
+            this.MiddelPanelDash.TabIndex = 2;
+            // 
+            // LogoGms
+            // 
+            this.LogoGms.AutoSize = true;
+            this.LogoGms.Font = new System.Drawing.Font("Montserrat", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogoGms.Location = new System.Drawing.Point(405, 20);
+            this.LogoGms.Name = "LogoGms";
+            this.LogoGms.Size = new System.Drawing.Size(119, 55);
+            this.LogoGms.TabIndex = 0;
+            this.LogoGms.Text = "GMS";
+            // 
+            // DashBoardBtn
+            // 
+            this.DashBoardBtn.BackColor = System.Drawing.Color.White;
+            this.DashBoardBtn.Font = new System.Drawing.Font("Montserrat", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DashBoardBtn.Location = new System.Drawing.Point(0, 58);
+            this.DashBoardBtn.Name = "DashBoardBtn";
+            this.DashBoardBtn.Size = new System.Drawing.Size(263, 75);
+            this.DashBoardBtn.TabIndex = 0;
+            this.DashBoardBtn.Text = "Dashboard";
+            this.DashBoardBtn.UseVisualStyleBackColor = false;
+            // 
+            // ClimateBtn
+            // 
+            this.ClimateBtn.BackColor = System.Drawing.Color.White;
+            this.ClimateBtn.Font = new System.Drawing.Font("Montserrat", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClimateBtn.Location = new System.Drawing.Point(0, 134);
+            this.ClimateBtn.Name = "ClimateBtn";
+            this.ClimateBtn.Size = new System.Drawing.Size(263, 75);
+            this.ClimateBtn.TabIndex = 1;
+            this.ClimateBtn.Text = "Climate";
+            this.ClimateBtn.UseVisualStyleBackColor = false;
+            // 
+            // ControlPanelBtn
+            // 
+            this.ControlPanelBtn.BackColor = System.Drawing.Color.White;
+            this.ControlPanelBtn.Font = new System.Drawing.Font("Montserrat", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ControlPanelBtn.Location = new System.Drawing.Point(0, 214);
+            this.ControlPanelBtn.Name = "ControlPanelBtn";
+            this.ControlPanelBtn.Size = new System.Drawing.Size(263, 75);
+            this.ControlPanelBtn.TabIndex = 2;
+            this.ControlPanelBtn.Text = "Control Panel";
+            this.ControlPanelBtn.UseVisualStyleBackColor = false;
+            // 
+            // ModalitetBtn
+            // 
+            this.ModalitetBtn.BackColor = System.Drawing.Color.White;
+            this.ModalitetBtn.Font = new System.Drawing.Font("Montserrat", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ModalitetBtn.Location = new System.Drawing.Point(0, 291);
+            this.ModalitetBtn.Name = "ModalitetBtn";
+            this.ModalitetBtn.Size = new System.Drawing.Size(263, 75);
+            this.ModalitetBtn.TabIndex = 3;
+            this.ModalitetBtn.Text = "Modalitet";
+            this.ModalitetBtn.UseVisualStyleBackColor = false;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.Controls.Add(this.MiddelPanelDash);
+            this.Controls.Add(this.LeftPanelDash);
+            this.Controls.Add(this.TopPanelDash);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.TopPanelDash.ResumeLayout(false);
+            this.TopPanelDash.PerformLayout();
+            this.LeftPanelDash.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanelDash;
+        private System.Windows.Forms.Label LogoGms;
+        private System.Windows.Forms.Panel LeftPanelDash;
+        private System.Windows.Forms.Button ModalitetBtn;
+        private System.Windows.Forms.Button ControlPanelBtn;
+        private System.Windows.Forms.Button ClimateBtn;
+        private System.Windows.Forms.Button DashBoardBtn;
+        private System.Windows.Forms.Panel MiddelPanelDash;
+    }
+}
