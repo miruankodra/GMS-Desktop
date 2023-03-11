5476f1d43f3968508f1c854777a8de598dbe31da
miruan <miruankodra@gmail.com>
2023-03-11 14:00:02 +0100

    Modality stylr

--- Modalities.Designer.cs
+++ Modalities.Designer.cs
@@ -45,7 +45,6 @@
             this.Templbl = new System.Windows.Forms.Label();
             this.Airlbl = new System.Windows.Forms.Label();
             this.Soillbl = new System.Windows.Forms.Label();
-            this._lbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -55,10 +54,9 @@
             // AddModBtn
             // 
             this.AddModBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AddModBtn.Location = new System.Drawing.Point(148, 132);
-            this.AddModBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.AddModBtn.Location = new System.Drawing.Point(111, 107);
             this.AddModBtn.Name = "AddModBtn";
-            this.AddModBtn.Size = new System.Drawing.Size(147, 135);
+            this.AddModBtn.Size = new System.Drawing.Size(110, 110);
             this.AddModBtn.TabIndex = 0;
             this.AddModBtn.Text = "+\r\n";
             this.AddModBtn.UseVisualStyleBackColor = true;
@@ -69,10 +67,9 @@
             this.AddLbl.AutoSize = true;
             this.AddLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AddLbl.ForeColor = System.Drawing.Color.White;
-            this.AddLbl.Location = new System.Drawing.Point(119, 288);
-            this.AddLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AddLbl.Location = new System.Drawing.Point(89, 234);
             this.AddLbl.Name = "AddLbl";
-            this.AddLbl.Size = new System.Drawing.Size(182, 31);
+            this.AddLbl.Size = new System.Drawing.Size(149, 25);
             this.AddLbl.TabIndex = 1;
             this.AddLbl.Text = "Add Modality";
             // 
@@ -89,10 +86,9 @@
             "Patate",
             "Domate",
             "Barishtore"});
-            this.comboBox1.Location = new System.Drawing.Point(477, 181);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Location = new System.Drawing.Point(358, 147);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(776, 47);
+            this.comboBox1.Size = new System.Drawing.Size(583, 39);
             this.comboBox1.TabIndex = 2;
             // 
             // label1
@@ -100,63 +96,67 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(471, 132);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(353, 107);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(231, 31);
+            this.label1.Size = new System.Drawing.Size(187, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Choose modality";
             // 
             // modalityname
             // 
             this.modalityname.AutoSize = true;
-            this.modalityname.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modalityname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.modalityname.ForeColor = System.Drawing.Color.Transparent;
-            this.modalityname.Location = new System.Drawing.Point(172, 430);
+            this.modalityname.Location = new System.Drawing.Point(129, 349);
+            this.modalityname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.modalityname.Name = "modalityname";
-            this.modalityname.Size = new System.Drawing.Size(159, 25);
+            this.modalityname.Size = new System.Drawing.Size(125, 18);
             this.modalityname.TabIndex = 4;
             this.modalityname.Text = "Modality Name:";
             // 
             // templabel
             // 
             this.templabel.AutoSize = true;
-            this.templabel.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.templabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.templabel.ForeColor = System.Drawing.Color.Transparent;
-            this.templabel.Location = new System.Drawing.Point(172, 486);
+            this.templabel.Location = new System.Drawing.Point(129, 395);
+            this.templabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.templabel.Name = "templabel";
-            this.templabel.Size = new System.Drawing.Size(137, 25);
+            this.templabel.Size = new System.Drawing.Size(108, 18);
             this.templabel.TabIndex = 5;
             this.templabel.Text = "Temperature:";
             // 
             // airhumiditylbl
             // 
             this.airhumiditylbl.AutoSize = true;
-            this.airhumiditylbl.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.airhumiditylbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.airhumiditylbl.ForeColor = System.Drawing.Color.Transparent;
-            this.airhumiditylbl.Location = new System.Drawing.Point(172, 541);
+            this.airhumiditylbl.Location = new System.Drawing.Point(129, 440);
+            this.airhumiditylbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.airhumiditylbl.Name = "airhumiditylbl";
-            this.airhumiditylbl.Size = new System.Drawing.Size(136, 25);
+            this.airhumiditylbl.Size = new System.Drawing.Size(103, 18);
             this.airhumiditylbl.TabIndex = 6;
             this.airhumiditylbl.Text = "Air Humidity:";
             // 
             // soilhumiditylbl
             // 
             this.soilhumiditylbl.AutoSize = true;
-            this.soilhumiditylbl.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soilhumiditylbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.soilhumiditylbl.ForeColor = System.Drawing.Color.Transparent;
-            this.soilhumiditylbl.Location = new System.Drawing.Point(172, 592);
+            this.soilhumiditylbl.Location = new System.Drawing.Point(129, 481);
+            this.soilhumiditylbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.soilhumiditylbl.Name = "soilhumiditylbl";
-            this.soilhumiditylbl.Size = new System.Drawing.Size(142, 25);
+            this.soilhumiditylbl.Size = new System.Drawing.Size(112, 18);
             this.soilhumiditylbl.TabIndex = 7;
             this.soilhumiditylbl.Text = "Soil Humidity:";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(125, 416);
+            this.pictureBox1.Location = new System.Drawing.Point(94, 338);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(41, 39);
+            this.pictureBox1.Size = new System.Drawing.Size(31, 32);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -164,9 +164,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(125, 472);
+            this.pictureBox2.Location = new System.Drawing.Point(94, 384);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(41, 39);
+            this.pictureBox2.Size = new System.Drawing.Size(31, 32);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
@@ -174,9 +175,10 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(125, 527);
+            this.pictureBox3.Location = new System.Drawing.Point(94, 428);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(41, 39);
+            this.pictureBox3.Size = new System.Drawing.Size(31, 32);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 10;
             this.pictureBox3.TabStop = false;
@@ -184,9 +186,10 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(125, 578);
+            this.pictureBox4.Location = new System.Drawing.Point(94, 470);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(41, 39);
+            this.pictureBox4.Size = new System.Drawing.Size(31, 32);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 11;
             this.pictureBox4.TabStop = false;
@@ -194,65 +197,57 @@
             // Namelbl
             // 
             this.Namelbl.AutoSize = true;
-            this.Namelbl.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Namelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Namelbl.ForeColor = System.Drawing.Color.Transparent;
-            this.Namelbl.Location = new System.Drawing.Point(381, 431);
+            this.Namelbl.Location = new System.Drawing.Point(286, 350);
+            this.Namelbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Namelbl.Name = "Namelbl";
-            this.Namelbl.Size = new System.Drawing.Size(63, 24);
+            this.Namelbl.Size = new System.Drawing.Size(49, 17);
             this.Namelbl.TabIndex = 12;
             this.Namelbl.Text = "Name";
             // 
             // Templbl
             // 
             this.Templbl.AutoSize = true;
-            this.Templbl.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Templbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Templbl.ForeColor = System.Drawing.Color.Transparent;
-            this.Templbl.Location = new System.Drawing.Point(381, 487);
+            this.Templbl.Location = new System.Drawing.Point(286, 396);
+            this.Templbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Templbl.Name = "Templbl";
-            this.Templbl.Size = new System.Drawing.Size(125, 24);
+            this.Templbl.Size = new System.Drawing.Size(101, 17);
             this.Templbl.TabIndex = 13;
             this.Templbl.Text = "Temperature";
             // 
             // Airlbl
             // 
             this.Airlbl.AutoSize = true;
-            this.Airlbl.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Airlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Airlbl.ForeColor = System.Drawing.Color.Transparent;
-            this.Airlbl.Location = new System.Drawing.Point(381, 542);
+            this.Airlbl.Location = new System.Drawing.Point(286, 440);
+            this.Airlbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Airlbl.Name = "Airlbl";
-            this.Airlbl.Size = new System.Drawing.Size(123, 24);
+            this.Airlbl.Size = new System.Drawing.Size(95, 17);
             this.Airlbl.TabIndex = 14;
             this.Airlbl.Text = "Air Humidity";
             // 
             // Soillbl
             // 
             this.Soillbl.AutoSize = true;
-            this.Soillbl.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Soillbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Soillbl.ForeColor = System.Drawing.Color.Transparent;
-            this.Soillbl.Location = new System.Drawing.Point(381, 593);
+            this.Soillbl.Location = new System.Drawing.Point(286, 482);
+            this.Soillbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Soillbl.Name = "Soillbl";
-            this.Soillbl.Size = new System.Drawing.Size(130, 24);
+            this.Soillbl.Size = new System.Drawing.Size(102, 17);
             this.Soillbl.TabIndex = 15;
             this.Soillbl.Text = "Soil Humidity";
             // 
-            // _lbl
-            // 
-            this._lbl.AutoSize = true;
-            this._lbl.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._lbl.ForeColor = System.Drawing.Color.Transparent;
-            this._lbl.Location = new System.Drawing.Point(512, 487);
-            this._lbl.Name = "_lbl";
-            this._lbl.Size = new System.Drawing.Size(17, 24);
-            this._lbl.TabIndex = 16;
-            this._lbl.Text = "-";
-            // 
             // Modalities
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.ClientSize = new System.Drawing.Size(1452, 814);
-            this.Controls.Add(this._lbl);
+            this.ClientSize = new System.Drawing.Size(1089, 661);
             this.Controls.Add(this.Soillbl);
             this.Controls.Add(this.Airlbl);
             this.Controls.Add(this.Templbl);
@@ -270,7 +265,7 @@
             this.Controls.Add(this.AddLbl);
             this.Controls.Add(this.AddModBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Modalities";
             this.Text = "Modalities";
             this.Load += new System.EventHandler(this.Modalities_Load);
@@ -301,6 +296,5 @@
         private System.Windows.Forms.Label Templbl;
         private System.Windows.Forms.Label Airlbl;
         private System.Windows.Forms.Label Soillbl;
-        private System.Windows.Forms.Label _lbl;
     }
 }
